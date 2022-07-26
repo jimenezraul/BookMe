3235f663313bf01716ef95975450c5284f1bdc0e
Raul <jimenezraul1981@gmail.com>
2022-07-25 22:38:27 -0400

    fixed issues with the dropdown menu and profile not closing when selecting link

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,6 +1,6 @@
-import { Button, Navbar, Form, Toggle, Dropdown } from "react-daisyui";
-const { Link } = require("react-router-dom");
-const { useState, useRef } = require("react");
+import { Button, Navbar, Form, Toggle, Dropdown, Menu } from "react-daisyui";
+import { Link } from "react-router-dom";
+const { useState } = require("react");
 const { useSelector, useDispatch } = require("react-redux");
 const { theme, setTheme } = require("../../features/theme/themeSlice");
 
@@ -24,6 +24,8 @@ const Menus = [
 ];
 
 const AppNavbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
   const dispatch = useDispatch();
   const appTheme = useSelector(theme);
   const [active, setActive] = useState(0);
@@ -34,16 +36,39 @@ const AppNavbar = () => {
     dispatch(setTheme(isDark ? "winter" : "night"));
   };
 
+  const handleMenuClick = (index) => {
+    setActive(index);
+    setIsOpen(false);
+  };
+
+  const handleClose = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleProfileClick = () => {
+    setActive(4);
+    setIsOpen2(!isOpen2);
+  };
+
+  const handleClose2 = () => {
+    setIsOpen2(!isOpen2);
+  };
+
   return (
     <div
       className={`flex w-full component-preview items-center justify-center gap-2 font-sans ${
         isDark && "bg-slate-800"
       } shadow-lg`}
     >
-      <Navbar>
+      <Navbar className='container'>
         <Navbar.Start>
           <Dropdown>
-            <Button color='ghost' shape='circle' tabIndex={0}>
+            <Button
+              color='ghost'
+              shape='circle'
+              onClick={handleClose}
+              onBlur={() => isOpen && setIsOpen(false)}
+            >
               <svg
                 xmlns='http://www.w3.org/2000/svg'
                 className='h-5 w-5'
@@ -59,24 +84,25 @@ const AppNavbar = () => {
                 />
               </svg>
             </Button>
-            <Dropdown.Menu
-              tabIndex={0}
-              className={`menu-compact w-52 border ${
-                isDark && "border-slate-900 bg-slate-800"
-              }  shadow-lg`}
-            >
-              {Menus.map((menu, index) => (
-                <Dropdown.Item
-                  key={index}
-                  tabIndex={0}
-                  onClick={() => setActive(index)}
-                  as={Link}
-                  to={menu.path}
-                >
-                  {menu.name}
-                </Dropdown.Item>
-              ))}
-            </Dropdown.Menu>
+            <div className={`${isOpen ? "opacity-100" : "opacity-0"}`}>
+              <Dropdown.Menu
+                className={`space-y-1 bg-white rounded-md menu-compact w-52 border ${
+                  isDark && "border-slate-900 bg-slate-800"
+                }  shadow-lg`}
+              >
+                {Menus.map((menu, index) => (
+                  <Menu.Item key={index}>
+                    <Link
+                      className={`${active === index && "active"}`}
+                      to={menu.path}
+                      onClick={() => handleMenuClick(index)}
+                    >
+                      {menu.name}
+                    </Link>
+                  </Menu.Item>
+                ))}
+              </Dropdown.Menu>
+            </div>
           </Dropdown>
         </Navbar.Start>
         <Navbar.Center>
@@ -87,38 +113,47 @@ const AppNavbar = () => {
         <Navbar.End className='navbar-end'>
           <div className='flex-none gap-2'>
             <Dropdown vertical='end'>
-              <Button color='ghost' className='avatar' shape='circle'>
+              <Button
+                color='ghost'
+                className='avatar'
+                shape='circle'
+                onClick={handleClose2}
+                onBlur={() => isOpen2 && setIsOpen2(false)}
+              >
                 <div className='w-10 rounded-full'>
-                  <img src='https://api.lorem.space/image/face?hash=33791' />
+                  <img
+                    src='https://api.lorem.space/image/face?hash=33791'
+                    alt=''
+                  />
                 </div>
               </Button>
-              <Dropdown.Menu
-                className={`w-52 menu-compact shadow-lg border ${
-                  isDark && "border-slate-900 bg-slate-800"
-                }`}
-              >
-                <li>
-                  <Link
-                    to='/profile'
-                    className={`justify-between ${
-                      active === 4 && "bg-base-200"
-                    }`}
-                    onClick={() => setActive(4)}
-                  >
-                    Profile
-                  </Link>
-                </li>
-                <Dropdown.Item>Logout</Dropdown.Item>
-                <Form className='bg-base-200 p-2 rounded-lg shadow'>
-                  <Form.Label title='DarkMode'>
-                    <Toggle
-                      className={`m-2 ${isDark && "bg-blue-700"}`}
-                      checked={isDark}
-                      onChange={handleThemeChange}
-                    />
-                  </Form.Label>
-                </Form>
-              </Dropdown.Menu>
+              <div className={`${isOpen2 ? "opacity-100" : "opacity-0"}`}>
+                <Dropdown.Menu
+                  className={`right-0 bg-white rounded-lg w-52 menu-compact shadow-lg border ${
+                    isDark && "border-slate-900 bg-slate-800"
+                  }`}
+                >
+                  <li>
+                    <Link
+                      to='/profile'
+                      className={`justify-between ${active === 4 && "active"}`}
+                      onClick={() => setActive(4)}
+                    >
+                      Profile
+                    </Link>
+                  </li>
+                  <Dropdown.Item>Logout</Dropdown.Item>
+                  <Form className='bg-base-200 p-2 rounded-lg shadow'>
+                    <Form.Label title='DarkMode'>
+                      <Toggle
+                        className={`m-2 ${isDark && "bg-blue-700"}`}
+                        checked={isDark}
+                        onChange={handleThemeChange}
+                      />
+                    </Form.Label>
+                  </Form>
+                </Dropdown.Menu>
+              </div>
             </Dropdown>
           </div>
         </Navbar.End>
